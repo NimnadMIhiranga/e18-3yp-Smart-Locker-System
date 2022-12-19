8ddd26014fd71324457764315848ea23416f1722
NisalaInduwara <e18266@eng.pdn.ac.lk>
2022-12-19 18:09:48 +0530

    completed sign into firebase

--- code/slocker/src/components/Header.js
+++ code/slocker/src/components/Header.js
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import '../css/home.css'
-import { Link } from 'react-router-dom'
-import {auth} from "../config/Config"
+import { Link,  useHistory } from 'react-router-dom'
+import { auth } from "../config/Config"
 
 
+export const Header = () =>  {
 
-export const Header = (props) =>  {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const [error, setError] = useState("")
+    const history = useHistory()
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-
-    const signin = (e) => {
-        e.preventDefault();
-        auth.signInWithEmailAndPassword(email, password).then(() => {
-            setEmail('');
-            setPassword('');
-            setError('');
-            props.history.push('/UserDashboard');
-        }).catch(err => setError(err.message));
-
-    }
+    async function signin(e) {
+        e.preventDefault()
+    
+        try {
+          setError("")
+          await 
+          auth.signInWithEmailAndPassword(emailRef.current.value, passwordRef.current.value)
+          history.push("/UserDashboard")
+        } catch {
+          setError("Failed to log in")
+        }
+      }
+    
     return (
         <div className="locker__header section__padding" id="home">
             <div className="locker__header-content">
@@ -37,12 +40,12 @@ export const Header = (props) =>  {
                 <label htmlFor="email" className='formtext'>Email</label>
                 <br />
                 <input type="email" className='form-control' required
-                onChange={(e) => setEmail(e.target.value)} value={email}/>
+                ref={emailRef}/>
                 
                 <label htmlFor="passowrd" className='formtext'>Password</label>
                 <br />
                 <input type="password" className='form-control' required
-                onChange={(e) => setPassword(e.target.value)} value={password}/>
+                ref={passwordRef}/>
                 <br /><br />
                 <button type="submit" className='submitbutton'>Sign in</button>
                 </form>
