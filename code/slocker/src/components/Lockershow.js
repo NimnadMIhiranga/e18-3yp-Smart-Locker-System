e750b21929c7df0c9cf7d0327b5200bcf67dbfd0
NisalaInduwara <e18266@eng.pdn.ac.lk>
2022-12-30 23:53:03 +0530

    location edit and delete

--- code/slocker/src/components/Lockershow.js
+++ code/slocker/src/components/Lockershow.js
@@ -11,26 +11,33 @@ export const Lockershow= ({ user, userID }) => {
   const { locations } = useContext(LocationContext);
   const [visible, setvisible] = useState(false);
   const [visible1, setvisible1] = useState(false);
+  const countref = useRef();
   //const countref = useRef();
-  const [count, setCount] = useState("");
-  const [key, setKey] = useState("");
   const [error, setError] = useState("");
 
-  async function editlocation(e) {
-    e.preventDefault();
+  async function deletelocation(id){
     try {
-            db.collection("Locations").doc(key).set({
-                Count: count,
-              }) .then(() => {
-                setKey("");
-                setCount("");
-              })
-              .catch((err) => setError(err.message));
-    } catch {
+      db.collection("Locations").doc(id).delete()
+      setvisible(false);
+    }
+    catch{
+      setError("Failed to delete");
+      setvisible(false);
+    }
+  }
+
+  async function editlocation(id){
+    try {
+      db.collection("Locations").doc(id).update({
+        Count:countref.current.value,
+      });
       setvisible1(false);
-  setError("Failed to edit the count");
-}
-}
+    }
+    catch{
+      setError("Failed to edit");
+      setvisible1(false);
+    }
+  }
 
 
   return (
@@ -46,12 +53,13 @@ export const Lockershow= ({ user, userID }) => {
                Number of lockers -  {location.LocationCount}
           </div>
           <div>
+            
           <button className='locker-go'>Go to lockers</button>
 
           <button className='locker-edit'onClick={()=>setvisible1(true)}>Edit count</button>
           <Model2 isOpen={visible1} onRequestClose={()=>setvisible1(false)}className='model1box'>
           <h1>Edit the locker count</h1><br/>
-          <form autoComplete="off" onSubmit={editlocation(location.LocationID)}>
+          <form autoComplete="off">
           <label htmlFor="passowrd" className="changepassword">
             Enter new count
           </label>
@@ -60,10 +68,9 @@ export const Lockershow= ({ user, userID }) => {
             type="count"
             className="form-password"
             required
-            onChange={(e) => setCount(e.target.value)}
-            value={count}
+            ref={countref}
           />
-          <button type="submit" className="edit-button">Edit</button>
+          <button type="submit" className="edit-button" onClick={()=>editlocation(location.LocationID)}>Edit</button>
           </form><br/>
           <button onClick={()=>setvisible1(false)} className="edit-cancel-button">Cancel</button>
           </Model2>
@@ -71,7 +78,7 @@ export const Lockershow= ({ user, userID }) => {
           <button className='locker-delete' onClick={()=>setvisible(true)}>Delete location</button>
           <Model1 isOpen={visible} onRequestClose={()=>setvisible(false)}className='model1box'>
           <p className='para'>Are you sure to delete this location?</p><br/>
-          <button type="submit" className="deletebutton">Delete</button>
+          <button type="submit" className="deletebutton" onClick={()=>deletelocation(location.LocationID)}>Delete</button>
           <button onClick={()=>setvisible(false)} className="cancel-button">Cancel</button>
           </Model1>
 
