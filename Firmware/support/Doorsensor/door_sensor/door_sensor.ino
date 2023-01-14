033708bfc383d77d629ba29daafdeda9221d11e7
NisalaInduwara <e18266@eng.pdn.ac.lk>
2023-01-14 16:34:52 +0530

    door sensor

--- Firmware/support/Doorsensor/door_sensor/door_sensor.ino
+++ Firmware/support/Doorsensor/door_sensor/door_sensor.ino
@@ -1,51 +1,104 @@
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <FirebaseArduino.h>
+//#include <BlynkSimpleEsp8266.h>
+#include <Firebase_ESP_Client.h>
+#include <Arduino.h>
 
-#define FIREBASE_HOST "slocker-6a0e7-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "SPhQWoIM4wj8z1oWsxH2mJCA9NFt1leLTPusnUaO"
-#define WIFI_SSID "Nisala Induwara"
-#define WIFI_PASSWORD "gango99ns"
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+#define DATABASE_URL "slocker-6a0e7-default-rtdb.firebaseio.com/" 
+#define API_KEY "AIzaSyAZ-1C80ncmZcB7ZreY3lienFpzc8jf3Ys"
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "Eng-Student"
+#define WIFI_PASSWORD "3nG5tuDt"
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "mcu1@gmail.com"
+#define USER_PASSWORD "12345678"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
 
 
-int sensor = 5;
+const int doorSensor = 5;
+int currentDoorState;
 
-int doorsensor(){
-  while(digitalRead(sensor) == 1){
-      digitalWrite(LED_BUILTIN, LOW); 
-      return 1;
+
+unsigned long sendDataPrevMillis = 0;
+
+int doorState(){
+   
+  currentDoorState = digitalRead(doorSensor);
+
+  if(currentDoorState == HIGH){
+      return 1;  
   }
-  while(digitalRead(sensor) != 1){
-    digitalWrite(LED_BUILTIN, HIGH);
-    return 0;
-  }
+
+  return 0;
+
+  
+   
 }
 
 void firebaseSetup(){
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-   //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-   Serial.print(Firebase.failed());
+   
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the user sign in credentials */
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    /* Assign the RTDB URL (required) */
+    config.database_url = DATABASE_URL;
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; 
+
+    #if defined(ESP8266)
+      fbdo.setBSSLBufferSize(2048, 2048 );
+    #endif
+
+    // Limit the size of response payload to be collected in FirebaseData
+    fbdo.setResponseSize(2048);
+
+    Firebase.begin(&config, &auth);
+
+    // Comment or pass false value when WiFi reconnection will control by your code or third party library
+    Firebase.reconnectWiFi(true);
+
+    Firebase.setDoubleDigits(5);
+
+    config.timeout.serverResponse = 10 * 1000;
 }
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(sensor, INPUT_PULLUP);
-  Serial.begin(9600);  
+  pinMode(doorSensor, INPUT_PULLUP);
+  Serial.begin(9600); 
   firebaseSetup();
   
   
 }
 
 void loop(){
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  if(doorsensor()==1){
-    Firebase.pushString("https://slocker-6a0e7-default-rtdb.firebaseio.com/doorsensor/status", "Opened");
-    Serial.print(Firebase.failed());
-    
-  }else if(doorsensor()==0){
-    Firebase.pushString("https://slocker-6a0e7-default-rtdb.firebaseio.com/doorsensor/status", "closed");  
-    Serial.print(Firebase.failed());
-     
+
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+      if(doorState()==1){
+        Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, F("doorsensor/state"), F("open")) ? "ok" : fbdo.errorReason().c_str());
+      }
+      else if(doorState()==0){
+        Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, F("doorsensor/state"), F("close")) ? "ok" : fbdo.errorReason().c_str());        
+      }
   }
-  delay(2000);
+
 }
