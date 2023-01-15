3c99bbeca74c9db5727a740b3e5b1e6176d510da
Nimnad Mihiranga <nimnadmihiranga1234@gmail.com>
2023-01-15 11:08:50 +0530

    integrated the component check and door sensor check

--- Firmware/support/Doorsensor/door_sensor/door_sensor.ino
+++ Firmware/support/Doorsensor/door_sensor/door_sensor.ino
@@ -1,5 +1,5 @@
 #include <ESP8266WiFi.h>
-//#include <BlynkSimpleEsp8266.h>
+// #include <BlynkSimpleEsp8266.h>
 #include <Firebase_ESP_Client.h>
 #include <Arduino.h>
 
@@ -9,7 +9,7 @@
 // Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
 
-#define DATABASE_URL "slocker-6a0e7-default-rtdb.firebaseio.com/" 
+#define DATABASE_URL "slocker-6a0e7-default-rtdb.firebaseio.com/"
 #define API_KEY "AIzaSyAZ-1C80ncmZcB7ZreY3lienFpzc8jf3Ys"
 
 /* 1. Define the WiFi credentials */
@@ -20,8 +20,8 @@
 #define USER_EMAIL "mcu1@gmail.com"
 #define USER_PASSWORD "12345678"
 
-String locationName= "nishmi";
-String lockerID= "1";
+String locationName = "nishmi";
+String lockerID = "1";
 String lockStatePath;
 
 // Define Firebase Data object
@@ -30,82 +30,82 @@ FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-
 const int doorSensor = 2;
 int currentDoorState;
 
-
 unsigned long sendDataPrevMillis = 0;
 
-int doorState(){
-   
+int doorState()
+{
+
   currentDoorState = digitalRead(doorSensor);
 
-  if(currentDoorState == HIGH){
-      return 1;  
+  if (currentDoorState == HIGH)
+  {
+    return 1;
   }
 
   return 0;
-
-  
-   
 }
 
-void firebaseSetup(){
-   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-   
-    /* Assign the api key (required) */
-    config.api_key = API_KEY;
+void firebaseSetup()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-    /* Assign the user sign in credentials */
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
 
-    /* Assign the RTDB URL (required) */
-    config.database_url = DATABASE_URL;
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
 
-    /* Assign the callback function for the long running token generation task */
-    config.token_status_callback = tokenStatusCallback; 
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
 
-    #if defined(ESP8266)
-      fbdo.setBSSLBufferSize(2048, 2048 );
-    #endif
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback;
 
-    // Limit the size of response payload to be collected in FirebaseData
-    fbdo.setResponseSize(2048);
+#if defined(ESP8266)
+  fbdo.setBSSLBufferSize(2048, 2048);
+#endif
 
-    Firebase.begin(&config, &auth);
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
 
-    // Comment or pass false value when WiFi reconnection will control by your code or third party library
-    Firebase.reconnectWiFi(true);
+  Firebase.begin(&config, &auth);
 
-    Firebase.setDoubleDigits(5);
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library
+  Firebase.reconnectWiFi(true);
 
-    config.timeout.serverResponse = 10 * 1000;
+  Firebase.setDoubleDigits(5);
+
+  config.timeout.serverResponse = 10 * 1000;
 }
 
-void setup(){
+void setup()
+{
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(doorSensor, INPUT_PULLUP);
-  Serial.begin(9600); 
+  Serial.begin(9600);
   firebaseSetup();
   lockStatePath = "Lockers/" + locationName + "/" + lockerID + "/LockState"; // to make the lock state path of the locker
-  
-  
 }
 
-void loop(){
+void loop()
+{
 
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1500 || sendDataPrevMillis == 0)){
-      sendDataPrevMillis = millis();
-      if(doorState()==1){ //when door is open
-        Serial.print("Door open----");
-        Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, (lockStatePath), F("1")) ? "ok" : fbdo.errorReason().c_str());
-      }
-      else if(doorState()==0){ //when door is closed
-        Serial.print("Door close----");
-        Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, (lockStatePath), F("0")) ? "ok" : fbdo.errorReason().c_str());        
-      }
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1500 || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+    if (doorState() == 1)
+    { // when door is open
+      Serial.print("Door open----");
+      Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, (lockStatePath), F("1")) ? "ok" : fbdo.errorReason().c_str());
+    }
+    else if (doorState() == 0)
+    { // when door is closed
+      Serial.print("Door close----");
+      Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, (lockStatePath), F("0")) ? "ok" : fbdo.errorReason().c_str());
+    }
   }
-
 }
